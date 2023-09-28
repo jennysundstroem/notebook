1b1ef169d4068e79064e90b87c277367e723b842
jennysundstroem <ejyuso-2@student.ltu.se>
2023-09-28 13:56:46 +0200

    tests added

--- my_first_extension/src/__tests__/myextension.spec.ts
+++ my_first_extension/src/__tests__/myextension.spec.ts
@@ -1,9 +1,38 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
+describe('My Extension', () => {
+  describe('Document Information Extension', () => {
+    it('should open the dropdown', async () => {
+      const dropdownMenu = document.querySelector('#dropdown-menu');
+      const fileMenu = dropdownMenu.querySelector('.file-menu'); 
+      fileMenu.click();
+      expect(dropdownMenu).toHaveClass('open');
+    });
 
-describe('myextension', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
+    it('should display Document Information button in dropdown menu', async () => {
+      const dropdownMenu = document.querySelector('#dropdown-menu');
+      const fileMenu = dropdownMenu.querySelector('.file-menu'); 
+      fileMenu.click();
+      const button = document.querySelector('#button-id'); 
+      expect(button).toContain('Document Information');
+    });
+
+    it('should open dialog when the Document Information button is clicked', async () => {
+      const dropdownMenu = document.querySelector('#dropdown-menu');
+      const fileMenu = dropdownMenu.querySelector('.file-menu'); 
+      fileMenu.click();
+      const button = document.querySelector('#button-id'); 
+      button.click();
+      const dialog = document.querySelector('#dialog-id');
+      expect(dialog).toBe;
+  });
+
+    it('should display the correct file information in dialog', () => {
+      const dropdownMenu = document.querySelector('#dropdown-menu');
+      const fileMenu = dropdownMenu.querySelector('.file-menu'); 
+      fileMenu.click();
+      const button = document.querySelector('#button-id'); 
+      button.click();
+      const dialog = document.querySelector('#dialog-id');
+      expect(dialog).toContain('mockedDataForResponse');
+    });
   });
 });
