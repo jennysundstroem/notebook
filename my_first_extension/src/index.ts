988ed234c966be8e922286f5a54d8be389549ec6
elvfor <elvfor-0@student.ltu.se>
2023-09-28 14:50:37 +0200

    Document Details button added. Kinda buggy atm

--- my_first_extension/src/index.ts
+++ my_first_extension/src/index.ts
@@ -2,34 +2,31 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
 import { ICommandPalette } from '@jupyterlab/apputils';
+import { FileMenu } from '@jupyterlab/mainmenu';
 import { showDialog, Dialog } from '@jupyterlab/apputils';
 import { NotebookPanel } from '@jupyterlab/notebook';
 
-console.log("test0")
-
-  async function makeApiRequest(path : string) {
-    try {
-      const apiURL = 'http://localhost:8888/api/contents/' + path
-      const response = await fetch(apiURL, {
-      method:'GET',
+async function makeApiRequest(path: string) {
+  try {
+    const apiURL = 'http://localhost:8888/api/contents/' + path;
+    const response = await fetch(apiURL, {
+      method: 'GET',
     });
-      if (!response.ok) {
-        // Handle non-OK response (e.g., 404 or 500)
-        throw new Error(`API request failed with status: ${response.status}`);
-        console.log('Tear')
-      }
-      const data = await response.json();
-      // Handle the response data here
-      console.log('API Response:', data);
-      return data
-    } catch (error) {
-      // Handle errors here
-      console.error('API Error:', error);
-
+    if (!response.ok) {
+      // Handle non-OK response (e.g., 404 or 500)
+      throw new Error(`API request failed with status: ${response.status}`);
+      console.log('Tear');
     }
+    const data = await response.json();
+    // Handle the response data here
+    console.log('API Response:', data);
+    return data;
+  } catch (error) {
+    // Handle errors here
+    console.error('API Error:', error);
   }
+}
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-apod',
@@ -42,34 +39,39 @@ const plugin: JupyterFrontEndPlugin<void> = {
     // Add an application command
     const command: string = 'apod:open';
     app.commands.addCommand(command, {
-
       label: 'Document Details',
       execute: async () => {
         const activeNotebook = app.shell.currentWidget as NotebookPanel;
         if (activeNotebook) {
           const notebookPath = activeNotebook.context.path;
-        let data = await makeApiRequest(notebookPath);
+          let data = await makeApiRequest(notebookPath);
           const result = await showDialog({
-          title: 'Document Details',
-          body: `
-          Document Name: ${JSON.stringify(data?.name)}. \r\n
-          Document Path: ${JSON.stringify(data?.path)}. \r\n
-          Created: ${JSON.stringify(data?.created)} \r\n
-          Last Modified: ${JSON.stringify(data?.last_modified)}
-          `,
+            title: 'Document Details',
+            body: `
+              Document Name: ${JSON.stringify(data?.name)}. \r\n
+              Document Path: ${JSON.stringify(data?.path)}. \r\n
+              Created: ${JSON.stringify(data?.created)} \r\n
+              Last Modified: ${JSON.stringify(data?.last_modified)}
+            `,
+            buttons: [Dialog.okButton({ label: 'OK' })],
+          });
 
-          buttons: [Dialog.okButton({ label: 'OK' })]
-        });
-
-        if (result.button.label === 'OK') {
-          console.log('OK button clicked');
+          if (result.button.label === 'OK') {
+            console.log('OK button clicked');
+          }
         }
-      }
-    }
+      },
     });
 
-    // Add the command to the palette.
+    // Add the command to the palette (optional)
     palette.addItem({ command, category: 'Document Details' });
-  }
+
+    // Add the "Document Details" button to the File menu
+    const fileMenu = new FileMenu({ commands: app.commands });
+    fileMenu.title.label = 'File';
+    fileMenu.addItem({ command });
+    app.shell.add(fileMenu, 'menu');
+  },
 };
+
 export default plugin;
